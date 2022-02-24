49f71ee83e09ba79fdc18459dd8a7d610d91dc40
BaptisS <56864846+BaptisS@users.noreply.github.com>
2022-02-24 11:09:42 +0100

    Update getfcpartners.sh

--- getfcpartners.sh
+++ getfcpartners.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-oci network fast-connect-provider-service list --compartment-id $compocid --all --region $1 --output json --query 'data[].{Partner:"provider-name",Service:"provider-service-name",Type:"type",vlan:"supported-virtual-circuit-types"}' > $1
-sed -i 's+]+],"Region":"'$1'"+g' $1
-cat $1 | jq '.[] | {Partner:."Partner",Service:.Service,Region:.Region,Type:.vlan}' | jq >> fcpartnersww.txt
+
+#oci network fast-connect-provider-service list --compartment-id $compocid --all --region $1 --output json --query 'data[].{Partner:"provider-name",Service:"provider-service-name",Type:"type",vlan:"supported-virtual-circuit-types"}' > $1
+#sed -i 's+]+],"Region":"'$1'"+g' $1
+#cat $1 | jq '.[] | {Partner:."Partner",Service:.Service,Region:.Region,Type:.vlan}' | jq >> fcpartnersww.txt
+
+oci network fast-connect-provider-service list --compartment-id $compocid --all --region $1 --output json --query 'data[].{Partner:"provider-name",Service:"provider-service-name",Type:"type",vlan:"supported-virtual-circuit-types"}' > ocifcpartners_$1
+sed -i 's+]+],"Region":"'$1'"+g' ocifcpartners_$1
+cat ocifcpartners_$1 | jq '.[] | {Partner:."Partner",Service:.Service,Region:.Region,Type:.vlan}' | jq >> fcpartnersww.txt
