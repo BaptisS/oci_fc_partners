2df3975e6b60091fe423dfa45d86091e149e8282
BaptisS <56864846+BaptisS@users.noreply.github.com>
2022-02-24 12:08:33 +0100

    Update fcpartners.sh

--- fcpartners.sh
+++ fcpartners.sh
@@ -9,22 +9,39 @@ rm -f getfcpartners.sh
 rm -f fcpartnersww.txt
 wget https://raw.githubusercontent.com/BaptisS/oci_fc_partners/main/getfcpartners.sh
 chmod +x getfcpartners.sh
-export compocid="'$2'"
+#export compocid="'$2'"
+#echo $compocid
+echo "We're currently updating the list of FC Partners available worldwide"
 regions=$(oci iam region list --query 'data[].{name:name}' | jq -r .[].name)
-for region in $regions; do ./getfcpartners.sh $region ; done
+for region in $regions; do ./getfcpartners.sh $region $2; done
 rm -f getfcpartners.sh
-;;
-location)
-echo Find Locations by Partner
-export partner="'$2'"
-locationsbypartner=$(cat fcpartnersww.txt | jq -r --arg partner "$partner" 'select(.Partner=="'$partner'") | [.Region]')
-echo $locationsbypartner | jq -r .[]
+echo "Usage : ./fcpartners.sh update 'compartment_ocid'" 
+echo "Usage : ./fcpartners.sh partner 'partner_name'"
+echo "Usage : ./fcpartners.sh location 'region_identifier'"  
 ;;
 partner)
-echo Find Partners by Location
-export location="'$2'"
-partnersbylocation=$(cat fcpartnersww.txt | jq -r 'select(.Region=="'$location'") | [.Partner]')
-echo $partnersbylocation | jq -r .[]
+echo FastConnect Partner:"'$2'" is available in the following OCI regions :
+#export partner="'$2'"
+export partner=$2
+locationsbypartner=$(cat fcpartnersww.txt | jq -r 'select(.Partner=="'"$partner"'") | [.Region]')
+echo $locationsbypartner | jq -r .[] | uniq | sort
+;;
+location)
+echo The following FastConnect Partners are available in OCI Region : "'$2'"
+export location=$2
+#export location='eu-frankfurt-1'
+#echo $2
+#echo $location
+#partnersbylocation=$(cat fcpartnersww.txt | jq -r --arg LOC "$location" 'select(.Region=="$LOC") | [.Partner]')
+partnersbylocation=$(cat fcpartnersww.txt | jq -r 'select(.Region=="'"$location"'") | [.Partner]')
+echo $partnersbylocation | jq -r .[] | uniq | sort
+#export pbl=$(echo $partnersbylocation | jq -r '.[]')
+#sort $pbl | uniq 
+;;
+list)
+echo "List all FastConnect partners available worldwide"
+partnerslist=$(cat fcpartnersww.txt | jq -r | [.Partner]')
+echo $partnerslist | jq -r .[] | uniq | sort
 ;;
 *)
 echo don\'t know
